a31673152beffab1c048bf836f314b01326eb0c4
Guy <guy12a@gmail.com>
2026-08-12 19:04:30 +0300

    UI design start

--- MomRequirements.py
+++ MomRequirements.py
@@ -1,32 +1,117 @@
 from customtkinter import *
 
-app = CTk()
-app.geometry("500x400")
+def reverse_by_spaces(text: str) -> str:
+    return " ".join(text.split(" ")[::-1])
 
-set_appearance_mode("light")
-set_default_color_theme("Greengage.json")
+class mandatoryFrame(CTkFrame):
+    def __init__(self,master):
+        super().__init__(master)
 
-checkbox = CTkCheckBox(master=app, text="Option", checkbox_height=20,checkbox_width=20)
-checkbox.place(relx = 0.5, rely = 0.2, anchor="center")
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=0)
 
-combobox = CTkComboBox(master=app, values=["options 1","options 2"])
-combobox.place(relx = 0.5, rely = 0.3, anchor="center")
+        self.title = CTkLabel(self,text=reverse_by_spaces("קורסי חובה"))
+        self.title.grid(row=0,column=0,padx=10, pady=(10, 0),columnspan=2)
 
-frame = CTkScrollableFrame(master=app,orientation="vertical")
-frame.pack(expand = True)
-frame.place(relx = 0.5, rely = 0.4, anchor="n")
+        self.boxNdLabel(reverse_by_spaces("מושגי יסוד בחקר סכסוכים"),1)
 
-label = CTkLabel(master=frame,text = "some text", font=("Arial",20))
-label.place(relx = 0.5, rely = 0.4, anchor="center")
+        self.boxNdLabel(reverse_by_spaces("מבנה חברתי של ישראל"),2)
+
+        self.boxNdLabel(reverse_by_spaces("סוגיות בקונפליקטים קבוצתיים וארגוניים"),3)
+
+        self.boxNdLabel(reverse_by_spaces("משא ומתן ככלי לניהול וישוב סכסוכים"),4)
+
+    def boxNdLabel(self,text,row):
+        self.lbl = CTkLabel(self,text=text)
+        self.lbl.grid(row=row, column=0, padx=(10,0), pady=(10, 0),sticky="e")
+
+        self.ckbox = CTkCheckBox(self,text="",width=10)
+        self.ckbox.grid(row=row, column=1, padx=(10,0), pady=(10, 0),sticky="e")
+
+class seminarFrame(CTkFrame):
+    def __init__(self,master):
+        super().__init__(master)
+
+        self.grid_columnconfigure(0,weight=1)
+        self.grid_columnconfigure(1,weight=1)
 
 
-def click_handler():
-    print(checkbox.get())
+        self.title = CTkLabel(self,text=reverse_by_spaces("סמינר"))
+        self.title.grid(row=0,column=0,padx=10, pady=(10, 0),sticky="new",columnspan=2)
 
-btn = CTkButton(master=frame, text="Save", command=click_handler,corner_radius=32)
-# anchor = must be n, ne, e, se, s, sw, w, nw, or center
-btn.place(relx=0.5,rely=0.5,anchor="center")
+        radio_var = IntVar(value=1)
+
+        self.radioNdLabel(reverse_by_spaces("לא ביצע"),1,radio_var)
+
+        self.radioNdLabel(reverse_by_spaces("התמודדות מתבגרים במצבי קונפליקט"),2,radio_var)
+
+        self.radioNdLabel(reverse_by_spaces("קונפליקטים במרחב המשפחתי"),3,radio_var)
+
+
+    def radioNdLabel(self,text,row,radio_var):
+        self.lbl = CTkLabel(self,text=text)
+        self.lbl.grid(row=row, column=0, padx=(10,0), pady=(10, 0),sticky="e")
+        self.seminarOption = CTkRadioButton(self,text="",variable= radio_var, value=row,width=10)
+        self.seminarOption.grid(row=row,column=1,padx=10, pady=(10, 0),sticky="e")
+
+class electivesFrame(CTkFrame):
+    def __init__(self,master):
+        super().__init__(master)
+
+        self.counter = 1
+        self.COLUMNS = ["Electives", "Credits"]
+
+        self.grid_columnconfigure((0,1),weight=1)
+
+        self.title = CTkLabel(self,text=reverse_by_spaces("קורסי בחירה"))
+        self.title.grid(row=0,column=0,padx=10, pady=(10, 0),sticky="new",columnspan=2)
+
+        self.addRow
+
+        self.addRowButton = CTkButton(self,text=reverse_by_spaces("הוסיפי שורה"),command=self.addRow)
+        self.addRowButton.grid(row=0,column=0)
+
+
+    def addRow(self):
+        self.entryName = CTkEntry(self, placeholder_text="name")
+        self.entryName.grid(row=self.counter, column=0)
+        self.entryCredits = CTkEntry(self, placeholder_text="credits")
+        self.entryCredits.grid(row=self.counter, column=1)
+        self.counter += 1
 
 
 
+
+class App(CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Mom's App")
+        self.geometry("600x500")
+        set_appearance_mode("light")
+        set_default_color_theme("blue")
+
+        self.grid_columnconfigure((0,1),weight=1)
+        self.grid_rowconfigure(0,weight=5)
+        self.grid_rowconfigure(1,weight=5)
+        self.grid_rowconfigure(2,weight=1)
+
+        self.seminarFrame = seminarFrame(self)
+        self.seminarFrame.grid(row=0,column = 0,padx=10, pady=(10, 0),sticky="nswe")
+
+        self.mandatoryFrame = mandatoryFrame(self)
+        self.mandatoryFrame.grid(row=0,column=1,padx=10, pady=(10, 0),sticky="nswe")
+
+        self.electivesFrame = electivesFrame(self)
+        self.electivesFrame.grid(row=1,column=0,padx=10,pady=(10,0),sticky="nsew")
+
+        self.button = CTkButton(self,text = reverse_by_spaces("שמירת קובץ"),command=self.save_button)
+        self.button.grid(row=2,column=0,padx=10,pady=10,sticky="nsew",columnspan=2)
+
+    def save_button(self):
+        print("saved")
+
+
+app = App()
 app.mainloop()
+
