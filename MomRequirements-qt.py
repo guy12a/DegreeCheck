d24fcb503f5e312e6b1fdda01578d92f3cc64cca
Guy <guy12a@gmail.com>
2026-08-14 00:30:05 +0300

    Designing QT Ui

--- MomRequirements-qt.py
+++ MomRequirements-qt.py
@@ -1,16 +1,80 @@
 from PySide6.QtCore import QSize, Qt
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PySide6.QtWidgets import QApplication, QCheckBox, QHBoxLayout, QLabel, QLineEdit, QMainWindow, QPushButton, QRadioButton, QVBoxLayout, QWidget
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Mom's App")
-        self.setFixedSize(QSize(500,400)) #can also use setMinimumSize or setMaximumSize
+        #self.setFixedSize(QSize(500,400)) #can also use setMinimumSize or setMaximumSize
+
+        layout = QVBoxLayout()
+        layout.addWidget(PersonalDetailsSection())
+        layout.addWidget(MandatorySection())
+        layout.addWidget(SeminarSection())
 
 
-        button = QPushButton("שלום לכם")
-        self.setCentralWidget(button)
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+class PersonalDetailsSection(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        label = QLabel("פרטים אישיים")
+        label.setAlignment(Qt.AlignCenter)
+
+        layout = QVBoxLayout()
+        layout.addWidget(label)
+        layout.addWidget(QLineEdit(placeholderText="הכניסי את שם הסטודנט"))
+        layout.addWidget(QLineEdit(placeholderText="הכניסי תאריך תחילת לימודים בחטיבה"))
+
+        
+        self.setLayout(layout)
+
+class SeminarSection(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        label = QLabel("סמינרים")
+        label.setAlignment(Qt.AlignCenter)
+
+        layout = QVBoxLayout()
+        layout.addWidget(label)
+        layout.addWidget(HebRadio("ללא"))
+        layout.addWidget(HebRadio("התמודדות מתבגרים במצבי קונפליקט"))
+        layout.addWidget(HebRadio("קונפליקטים במרחב המשפחתי"))
+
+        self.setLayout(layout)
+
+class MandatorySection(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        label = QLabel("קורסי חובה")
+        label.setAlignment(Qt.AlignCenter)
+
+        layout = QVBoxLayout()
+        layout.addWidget(label)
+        layout.addWidget(HebCheckbox("מושגי יסוד בחקר סכסוכים"))
+        layout.addWidget(HebCheckbox("מבנה חברתי של ישראל"))
+        layout.addWidget(HebCheckbox("סוגיות בקונפליקטים קבוצתיים וארגוניים"))
+        layout.addWidget(HebCheckbox("משא ומתן ככלי לניהול וישוב סכסוכים"))
+
+        self.setLayout(layout)
+
+
+class HebCheckbox(QCheckBox):
+    def __init__(self,text):
+        super().__init__(text)
+        self.setLayoutDirection(Qt.RightToLeft)
+
+class HebRadio(QRadioButton):
+    def __init__(self,text):
+        super().__init__(text)
+        self.setLayoutDirection(Qt.RightToLeft)
+
 
 
 app = QApplication([]) #manages event loop
